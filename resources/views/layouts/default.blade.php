47092d26668b3636fe379a630e814d41b563db08
vagrant <vagrant@homestead>
2018-03-01 10:01:31 +0000

    login

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,11 +5,12 @@
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @include('layouts._header')
+@include('layouts._header')
 <div class="container">
     @include('shared._message')
     @yield('content')
     @include('layouts._footer')
 </div>
+<script src="/js/app.js"></script>
 </body>
 </html>
